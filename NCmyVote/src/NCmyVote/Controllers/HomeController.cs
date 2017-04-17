2da5341753f885d96d07320de748b38317ed9f6a
YinLearnProgramming <hangprogramming@gmail.com>
2017-04-16 20:44:50 -0400

    this is my first time doing c# and net with VB2015

---
+++ NCmyVote/src/NCmyVote/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NCmyVote.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "The object of this application";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult VotingLocation()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+
+        }
+
+        public IActionResult Candidates()
+        {
+            return View();
+        }
+
+        public IActionResult Information()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
